1f16b20d4609398fb4546111d0ad4375ddce42e4
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2019-01-22 19:49:53 +0100

    nothing

--- server.js
+++ server.js
@@ -8,14 +8,25 @@ app.use(bodyparser.json())
 
 const auth = require('./server/routes/auth')
 const home=require('./server/routes/home')
-const login=require('./server.router/login')
+const login=require('./server/routes/login')
 const register=require('./server/routes/register')
 const ajouterAdmin=require('./server/routes/dashboard/Admin/ajouterAdmin')
 const effacerUser=require('./server/routes/dashboard/Admin/effacerUser')
 const listerUser=require('./server/routes/dashboard/Admin/listerUser')
+const ajouterComment = require('./server/routes/article/commentaire/ajouterComment')
+const deleteComment = require('./server/routes/article/commentaire/deleteComment')
+const listerComment = require('./server/routes/article/commentaire/listercomments')
+const updateComment = require('./server/routes/article/commentaire/updateComment')
+const ajouterArt = require('./server/routes/article/ajouterArt')
+const consulterArt = require('./server/routes/article/consulterArt')
+const listerArticles = require ('./server/routes/article/listerArticles')
+const suprimerArt = require ('./server/routes/article/suprimerArt')
+const updateArt = require ('./server/routes/article/updateArt')
+
 app.use('/auth', auth)
+
 
 app.listen(3001, (err)=>{
     if(err)throw err;
-    console.log('server is running on port 3000')
+    console.log('server is running on port 3001')
 })
