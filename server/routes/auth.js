129529c9bb30a6611f04ee13ebc2c4fe47e51e83
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2019-01-22 18:44:09 +0100

    ok

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 
 router.post('/register', async (req, res) => {
    res.send('')
-});
+}); 
 
 router.post('/users', async (req, res) => {
     const result = await User.find().exec()
