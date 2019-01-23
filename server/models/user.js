c3ca17ed9be8afb2225ea18962a35b805a52e3de
Khalilmnasri <khalilmnassriqnet@gmail.com>
2019-01-23 20:54:08 +0100

    ajoutAdmin

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,8 @@ const user = new mongoose.Schema({
     name: String,
     lastname: String,
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 5 }
+    password: { type: String, required: true, minlength: 5 },
+    admin: {type: Boolean, default: false }
 });
 
 const userModel = mongoose.model('users',user);
