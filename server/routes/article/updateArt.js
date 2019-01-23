233ec27a13ae56664a3a07cc19caaeaad4b8ff3c
mahaZr <maha2074@hotmail.com>
2019-01-23 19:06:24 +0100

    mmm

--- server/routes/article/updateArt.js
+++ server/routes/article/updateArt.js
@@ -1,7 +1,10 @@
 
 const router = require('express').Router();
 
-router.post('article/updateArt/:idUser/:idArt', async (req,res)=>{
+router.post('/updateArt/:idUser/:idArt', async (req,res)=>{
+    var idArt=req.params.idArt;
+    var idUser=req.params.idUser;
+    const resultat=await article.updateOne()
     res.send()
 })
 module.exports = router;
