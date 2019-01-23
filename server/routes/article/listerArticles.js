7e247a76a82c112dad2f882c52397d2a859f43a9
mahaZr <maha2074@hotmail.com>
2019-01-23 21:22:29 +0100

    maha change

--- server/routes/article/listerArticles.js
+++ server/routes/article/listerArticles.js
@@ -1,5 +1,32 @@
-const router=require('express').Router();
+const router = require('express').Router();
+const article = require('../../models/article');
+const user = require('../../models/user');
 
-router.get('article/ListerArts/:idArt/:idUser/:idAut',async (req,res)=>{
-    res.send()
+router.get('/listerArts/:idAut', async (req, res) => {
+
+    const idaut = req.params.idAut;
+    var resultat;
+    if (idaut == 'all') {
+        resultat = await article.find().exec();
+        console.log(resultat);
+        res.send(resultat);
+    } else {
+        try {
+            var ketib = await user.findById(idaut).exec();
+            console.log(ketib);
+        } catch (error) {
+            res.send('aucun auteur correspondant a ce nom');
+        }
+
+        if (!ketib) {
+            res.send('aucun auteur correspondant a ce nom');
+
+        } else {
+            resultat = await article.find({ auteur: ketib.name }).exec();
+            res.send(resultat);
+        }
+
+    }
 })
+
+module.exports = router;
