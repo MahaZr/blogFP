31a9a0f75ea551b9746a0fe6295dd5753154e0b2
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2019-01-24 16:05:54 +0100

    finish comments

--- server/routes/article/commentaire/updateComment.js
+++ server/routes/article/commentaire/updateComment.js
@@ -1,5 +1,25 @@
-const router=require('express').Router();
+const router = require('express').Router();
+const users = require('../../../models/user');
+const comments = require('../../../models/comment');
 
-router.post('article/commentaire/UpdateComment/:idUser/:idArt', async (req,res)=>{
-    res.send()
-})
+router.post('/UpdateComment/:idUser/:idcomment', async (req, res) => {
+    try {
+        var user = await users.findById(req.params.idUser).exec();
+    } catch (error) {
+        res.send('false id user');
+    }
+    try {
+        var comment = await comments.findById(req.params.idcomment).exec();
+    } catch (error) {
+        res.send('false id comment');
+    }
+    if (user.admin || (comment.IdUser == req.params.idUser)) {
+        await comments.where({ _id: req.params.idcomment }).updateOne({ $set: { corps: req.body.corps } });
+        const resultat = await comments.findById(req.params.idcomment).exec();
+        res.send(resultat);
+    } else {
+        res.send('you are not allowd to modif this comment');
+    }
+});
+
+module.exports = router ;
