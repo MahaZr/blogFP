a594e438ceb9a018fe2c9393dbe28da6265f2385
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2019-01-31 20:58:40 +0100

    jwt1

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,14 +1,21 @@
 const router=require('express').Router();
 const user = require('../models/user');
+const jwt = require('jsonwebtoken').authenticate;
 
 
 router.post('/login', async (req,res)=>{
 
     const result = await  user.findOne({ "email": req.body.email }).exec()
     if(result!= null ){
+       
+        
     if (result.password === req.body.password) {
-        res.send(result);
+        jwt.sign({user : result}, 'secretKey',(err,token)=>{
+            res.send({token : token});
+        });
+
     } else {
+
         res.status(200).send('bad pass');
     }
 }
