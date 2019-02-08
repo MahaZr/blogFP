4e21ee2ce1f121bfcb2751861797204825347c8c
Khalilmnasri <khalilmnassriqnet@gmail.com>
2019-02-08 19:03:46 +0100

    verfiy token

--- server/routes/article/commentaire/listercomments.js
+++ server/routes/article/commentaire/listercomments.js
@@ -1,8 +1,9 @@
 const router=require('express').Router();
 const comments = require('../../../models/comment');
+const verifyToken = require('../../jwt').verifyToken;
 
 
-router.get('/listercomment/:idArt', async (req,res)=>{
+router.get('/listercomment/:idArt',verifyToken, async (req,res)=>{
  var results = await comments.find({ IDArticle : req.params.idArt}).exec();
  res.send(results);
 });
